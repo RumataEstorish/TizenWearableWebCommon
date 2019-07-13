3a5183bd23a37bf89670ed813ccf863122e8f7b7
Rumata <rumataestorish@yandex.ru>
2019-07-13 19:16:57 +0300

    Commented log

--- js/log-1.0.1.1.js
+++ js/log-1.0.1.1.js
@@ -2,13 +2,28 @@
 /*jshint multistr: true */
 
 function Log(){
-	
 }
 
+/**
+ * Global property enables write Log.debug
+ */
 Log.DEBUG = false;
+
+/**
+ * Set dom item to write log into
+ */
 Log.ITEM = null;
+
+/**
+ * If set to true, will always launch alert on errors
+ */
 Log.FORCE_ERROR_ALERT = false;
 
+/**
+ * Write info to log
+ * @param e. Log data
+ * @param al. If set, alert info
+ */
 Log.info = function(e, al){
 	console.info(e);
 	if (al){
@@ -20,6 +35,11 @@ Log.info = function(e, al){
 	}
 };
 
+/**
+ * Write warning to log
+ * @param e. Log data
+ * @param al. If set, alert warning
+ */
 Log.warn = function(e, al){
 	console.warn(e);
 	if (al){
@@ -31,6 +51,11 @@ Log.warn = function(e, al){
 	}
 };
 
+/**
+ * Write error to log
+ * @param e. Log data
+ * @param silent. If not set, alert on error
+ */
 Log.error = function(e, silent){
 	console.error(e);
 	if (!silent || Log.FORCE_ERROR_ALERT === true){
@@ -42,6 +67,10 @@ Log.error = function(e, silent){
 	}
 };
 
+/**
+ * Write debug to log
+ * @param e. Log data
+ */
 Log.debug = function(e){
 	if (!Log.DEBUG){
 		return;
@@ -53,19 +82,37 @@ Log.debug = function(e){
 	}
 };
 
+/**
+ * Write debug to log
+ * @param e. Log data
+ */
 Log.d = function(e){
 	Log.debug(e);
 };
 
-
+/**
+ * Write error to log
+ * @param e. Log data
+ * @param silent. If set, no alert on error
+ */
 Log.e = function(e, silent){
 	Log.error(e, silent);
 };
 
+/**
+ * Write warning to log
+ * @param e. Log data
+ * @param al. If set, alert on warning
+ */
 Log.w = function(e, al) {
 	Log.warn(e, al);
 };
 
+/**
+ * Write info to log
+ * @param e. Log data
+ * @param al. If set, alert on info
+ */
 Log.i = function(e, al){
 	Log.info(e, al);
 };
