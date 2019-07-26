4173ce53d0753c7a0b7891d8553e0225fc911c7e
Rumata <rumataestorish@yandex.ru>
2019-07-26 11:47:36 +0300

    Fixed toast

--- js/ui/toastMessage-1.1.1.js
+++ js/ui/toastMessage-1.1.1.js
@@ -1,12 +1,15 @@
 /*global $, tau*/
 
 /*
+ * v1.1.1
+ * close delay increased to 2 sec
+ * fixed not showing
  * v1.0.1
  * added click on popup to close it
  * popup closing after 1500ms
  * */
 
-ToastMessage.CLOSE_DELAY = 1500;
+ToastMessage.CLOSE_DELAY = 2000;
 
 /**
  * Constructor of class ToastMessage
@@ -14,7 +17,7 @@ ToastMessage.CLOSE_DELAY = 1500;
  * @param popupContent - popup content element name on page. Should be unique.
  */
 function ToastMessage(popupName, popupContent) {
-	var isOpened = false, self = this;
+	var isOpened = false, self = this, pName = popupName[0] === '#'? popupName.slice(1) : popupName, pContent = popupContent[0] === '#' ? popupContent.slice(1) : popupContent;
 
 	Object.defineProperty(this, "isOpened", {
 		get : function() {
@@ -27,18 +30,22 @@ function ToastMessage(popupName, popupContent) {
 
 	Object.defineProperty(this, "popupName", {
 		get : function() {
-			return popupName;
+			return pName;
 		}
 	});
 
 	Object.defineProperty(this, "popupContent", {
 		get : function() {
-			return popupContent;
+			return pContent;
 		}
 	});
+	
 	$(this.popupName).on("popuphide", function() {
 		self.isOpened = false;
 	});
+	
+	
+	$('body').append('<div id="' + pName + '" class="ui-popup ui-popup-toast"><div id="'+ pContent +'" class="ui-popup-content"	style="overflow-y: no-scroll;"></div></div>');
 }
 
 /**
@@ -53,22 +60,23 @@ ToastMessage.prototype.show = function(txt, delay) {
 	}
 
 	$(this.popupName).on("click", function(){
-		tau.closePopup(self.popupName);
+		tau.closePopup('#' + self.popupName);
 	});
 	
 	setTimeout(function(){
-		tau.closePopup(self.popupName);
+		tau.closePopup('#' + self.popupName);
+		self.isOpened = false;
 	}, ToastMessage.CLOSE_DELAY);
 	
-	$(this.popupContent).html(txt);
+	$('#' + this.popupContent).html(txt);
 	if (this.isOpened !== true) {
 		if (delay) {
 			setTimeout(function() {
-				tau.openPopup(self.popupName);
+				tau.openPopup('#' + self.popupName);
 				self.isOpened = true;
 			}, delay);
 		} else {
-			tau.openPopup(this.popupName);
+			tau.openPopup('#' + this.popupName);
 			this.isOpened = true;
 		}
 	}
