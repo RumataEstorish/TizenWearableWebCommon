b8bb3bd1332501881cf3a8a45bc42699e0486ab4
Rumata <RumataEstorish@yandex.ru>
2020-01-15 13:42:45 +0300

    Swipe list documented

--- js/ui/swipeList-1.0.0.1.js
+++ js/ui/swipeList-1.0.0.1.js
@@ -2,6 +2,12 @@
 /*jshint unused: false*/
 /*jslint laxbreak: true*/
 
+/**
+ * SwipeList constructor
+ * @param pageName - name of page where swipe list is placed
+ * @param leftSwipe - function called when swipe left performed
+ * @param rightSwipe - function called when swipe right performed
+ */
 function SwipeList(pageName, leftSwipe, rightSwipe) {
 	var pName = pageName[0] === '#' ? pageName : '#' + pageName, swipeList = null, swipeListList = $(pName
 			+ ' .ui-swipelist-list')[0];
